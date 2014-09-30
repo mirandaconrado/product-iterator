cfc06794f6ba703a3feff98f9d88e153dc906557
Conrado Miranda <miranda.conrado@gmail.com>
2014-09-30 10:28:58 -0300

    Updated documentation to include proxy.

--- include/iterator_proxy.hpp
+++ include/iterator_proxy.hpp
@@ -1,3 +1,24 @@
+// This file defines a fake container that is only defined by a pair of
+// iterators.
+//
+// The product_iterator is only able to accept containers to simplify the
+// building process, as it would be necessary to detect if a given argument is a
+// container or an iterator. However, since only containers are allowed,
+// iterator-only sequences, such as those defined by boost::couting_iterator
+// couldn't be used. Hence a fake container for these iterators needed to be
+// defined.
+//
+// The iterator_proxy is defined only by its begin and end iterators and isn't
+// able to distinguish between iterator and const_iterator, so it's up to the
+// user to know wether the iterator from begin() or end() is actually a
+// const_iterator.
+//
+// Example:
+// vector<int> c1({1,2});
+// auto c2 = make_iterator_proxy(c1.begin(), c2.end());
+// for (auto v : c2)
+//   // do stuff
+
 #ifndef __ITERATOR_PROXY_HPP__
 #define __ITERATOR_PROXY_HPP__
 
